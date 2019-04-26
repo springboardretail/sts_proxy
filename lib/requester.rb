2460b02dc4509304bef0687432e1170a335b7e4a
Rui Santos <anderson.rui@gmail.com>
2019-04-26 17:11:02 -0300

    Use HTTP persistent connection and ensure one connection per thread

--- lib/requester.rb
+++ lib/requester.rb
@@ -2,6 +2,15 @@
 # Utility class which creates a request
 class Requester
   class << self
+    def http_connection(endpoint)
+      if !Thread.current.thread_variable?(:connections_by_endpoint)
+        Thread.current.thread_variable_set(:connections_by_endpoint, {})
+      end
+
+      connections = Thread.current.thread_variable_get(:connections_by_endpoint)
+      connections[endpoint] ||= Excon.new(endpoint, persistent: true)
+    end
+
     ##
     # Sends a POST request to the given address and returns a
     # hash containing the parsed response body and the raw Patron response
@@ -10,12 +19,12 @@ class Requester
     # @param endpoint [String] address
     # @param body [String] data to be sent
     # @param content_type [String]
-    # @return [Hash] Patron response
+    # @return [Hash] Excon response
     def request(endpoint, body, content_type)
-      Excon.post(endpoint, {
+      http_connection(endpoint).post(
         body: body,
         headers: { 'Content-Type' => content_type }
-      })
+      )
     end
   end
 end
